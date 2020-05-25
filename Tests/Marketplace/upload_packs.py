b49c03314a1e1ae75ddb1660da2aada8c350517e
dtavori <dtavori@paloaltonetworks.com>
2020-05-25 20:39:44 +0300

    small fix

--- Tests/Marketplace/upload_packs.py
+++ Tests/Marketplace/upload_packs.py
@@ -402,10 +402,11 @@ def load_json(file_path):
         dict: loaded json file.
 
     """
+    from time import sleep
     for i in range(10):
         if os.path.isfile(file_path):
             break
-        time.sleep(1)
+        sleep(1)
     with open(file_path, 'r') as json_file:
         result = json.load(json_file)
 
