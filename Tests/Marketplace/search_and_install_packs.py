448e87b8dd67bcac0e4cfebfddaca549e4e6f6e2
dtavori <dtavori@paloaltonetworks.com>
2020-05-25 18:59:36 +0300

    debugging

--- Tests/Marketplace/search_and_install_packs.py
+++ Tests/Marketplace/search_and_install_packs.py
@@ -217,12 +217,12 @@ def search_pack_and_its_dependencies(client, prints_manager, pack_id, packs_to_i
         lock.release()
 
 
-def add_base_pack_to_installation_request(installation_request_body):
-    with open('./Packs/Base/pack_metadata.json', 'r') as json_file:
+def add_pack_to_installation_request(pack_id, installation_request_body):
+    with open('./Packs/{}/pack_metadata.json'.format(pack_id), 'r') as json_file:
         pack_metadata = json.load(json_file)
         version = pack_metadata.get('currentVersion')
         installation_request_body.append({
-            'id': 'Base',
+            'id': pack_id,
             'version': version
         })
 
@@ -239,9 +239,10 @@ def search_and_install_packs_and_their_dependencies(pack_ids, client, prints_man
     lock = Lock()
     threads_list = []
 
-    packs_to_install = []  # we save here all the packs we want to install, to avoid duplications
+    packs_to_install = ['Base', 'DeveloperTools']  # we save all the packs we want to install, to avoid duplications
     installation_request_body = []  # the packs to install, in the request format
-    add_base_pack_to_installation_request(installation_request_body)
+    for pack_id in packs_to_install:
+        add_pack_to_installation_request(pack_id, installation_request_body)
 
     host = client.api_client.configuration.host
     msg = 'Starting to search and install packs in server: {}\n'.format(host)
